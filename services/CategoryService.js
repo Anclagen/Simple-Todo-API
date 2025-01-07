6c5efe0574432df3a7f323f91e583ecc06f78a8a
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-01-07 02:00:14 +0100

    category service

---
+++ services/CategoryService.js
@@ -0,0 +1,37 @@
+class CategoryService {
+  constructor(db) {
+    this.client = db.sequelize;
+    this.Category = db.Category;
+    this.User = db.User;
+  }
+
+  async create(name, userId) {
+    return this.Category.create({
+      name,
+      userId,
+    });
+  }
+
+  async findAll() {
+    return this.Category.findAll();
+  }
+
+  async findOne(id) {
+    return this.Category.findByPk(id);
+  }
+
+  async update(id, name) {
+    return this.Category.update(
+      { name },
+      {
+        where: { id },
+      }
+    );
+  }
+
+  async delete(id) {
+    return this.Category.destroy({
+      where: { id },
+    });
+  }
+}
