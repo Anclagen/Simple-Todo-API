d42d181e1059a0c62c2eba66a4020bf94ab1ff72
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-01-09 23:48:15 +0100

    added swagger models

--- swagger.js
+++ swagger.js
@@ -1,19 +1,80 @@
-const Category = require("./models/Category");
-
 const swaggerAutogen = require("swagger-autogen")();
+
 const doc = {
   info: {
     version: "1.0.0",
     title: "My TODOs",
     description: "A simple TODO app with user authentication and categories",
   },
+  // If the app is deployed, update this host to your production URL.
   host: "localhost:3000",
+  // Define the security scheme for Bearer token
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+  },
   definitions: {
+    User: {
+      id: 1,
+      name: "John Doe",
+      email: "example@example.com",
+      password: "password",
+    },
+    UserBodySignUp: {
+      name: "John Doe",
+      email: "example@example.com",
+      password: "password",
+    },
+    UserBodyLogin: {
+      email: "example@example.com",
+      password: "password",
+    },
     Category: {
       id: 1,
-      name: "Category Name",
+      name: "Work",
       UserId: 1,
     },
+    CategoryBody: {
+      name: "New/Updated Category",
+    },
+    Todo: {
+      id: 1,
+      name: "Buy Groceries",
+      description: "Get fruits and vegetables",
+      StatusId: 1,
+      CategoryId: 1,
+      UserId: 1,
+    },
+    TodoResponse: {
+      id: 1,
+      name: "Buy Groceries",
+      description: "Get fruits and vegetables",
+      StatusId: 1,
+      CategoryId: 1,
+      UserId: 1,
+      Category: {
+        $ref: "#/definitions/Category",
+      },
+      Status: {
+        $ref: "#/definitions/Status",
+      },
+    },
+    TodoBody: {
+      name: "New/Updated Todo",
+      description: "New/Updated Description",
+      StatusId: 1,
+      CategoryId: 1,
+    },
+    Status: {
+      id: 1,
+      status: "In Progress",
+    },
+
+    // etc...
   },
 };
 
