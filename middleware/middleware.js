b4bb673aac21dd6bc469477b6bd77068a506a76d
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-01-09 23:48:39 +0100

    added todo middleware validators

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -2,13 +2,15 @@ var jwt = require("jsonwebtoken");
 var db = require("../models");
 var CategoryService = require("../services/CategoryService");
 var categoryService = new CategoryService(db);
+var TodoService = require("../services/TodoService");
+var todoService = new TodoService(db);
 
 // Middleware function to determine if the API endpoint request is from an authenticated user
 function isAuth(req, res, next) {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      // #swagger.responses[401] = {description: 'Unauthorized, malformed authorization header', schema: {status:"fail", data: {statusCode: 401, result: "Unauthorized, malformed authorization header"}}}
+      // #swagger.responses[401] = {description: 'Unauthorized, malformed or missing authorization header', schema: {status:"fail", data: {statusCode: 401, result: "Unauthorized, malformed authorization header"}}}
       return res.status(401).jsend.fail({
         data: { statusCode: 401, result: "Unauthorized, malformed authorization header" },
       });
@@ -42,7 +44,7 @@ async function isCategoryOwner(req, res, next) {
 
     // Check if the user is the owner of the category
     if (category.UserId !== req.user.id) {
-      // #swagger.responses[403] = {description: 'You are not authorized to update this category', schema: {status: "fail", data: {statusCode: 403, result: "You are not authorized to update this category"}}}
+      // #swagger.responses[403] = {description: 'You are not authorized to update this entity', schema: {status:"fail", data: {statusCode: 403, result: "You are not authorized to update this category"}}}
       return res.status(403).jsend.fail({ statusCode: 403, result: "You are not authorized to update this category" });
     }
 
@@ -53,7 +55,86 @@ async function isCategoryOwner(req, res, next) {
   }
 }
 
+async function validateBody(req, res, next) {
+  try {
+    const { name, description, statusId, categoryId } = req.body;
+    if (!name || !description || !statusId || !categoryId) {
+      // #swagger.responses[400] = {description: 'Missing name, description, statusId or categoryId in request body', schema: {status: "fail", data: {statusCode: 400, result: "Missing name, description, statusId or categoryId in request body"}}}
+      return res.status(400).jsend.fail({ statusCode: 400, result: "Missing name, description, statusId or categoryId in request body" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(500).jsend.error({ status: "error", message: "Internal server error", data: error });
+  }
+}
+
+// Optional check for statusId, place after validateTodoBody for creating a todo
+async function validateCategory(req, res, next) {
+  const { categoryId } = req.body;
+
+  // If no categoryId is provided, move to the next middleware
+  if (!categoryId) {
+    next();
+  }
+
+  const category = await categoryService.findOne(categoryId);
+  if (!category) {
+    // #swagger.responses[404] = {description: 'Category not found', schema: {status: "fail", data: {statusCode: 404, result: "Category not found"}}}
+    return res.status(404).jsend.fail({ statusCode: 404, result: "Category not found" });
+  }
+
+  if (category.UserId !== req.user.id) {
+    return res.status(403).jsend.fail({ statusCode: 403, result: "You are not authorized to update this category" });
+  }
+
+  next();
+}
+
+// Optional check for statusId, place after validateTodoBody for creating a todo
+async function validateStatus(req, res, next) {
+  try {
+    const { statusId } = req.body;
+
+    // If no statusId is provided, move to the next middleware
+    if (!statusId) {
+      next();
+    }
+
+    const status = await todoService.findOneStatus(statusId);
+    if (!status) {
+      // #swagger.responses[404] = {description: 'Status not found', schema: {status: "fail", data: {statusCode: 404, result: "Status not found"}}}
+      return res.status(404).jsend.fail({ statusCode: 404, result: "Status not found" });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).jsend.error({ status: "error", message: "Internal server error", data: error });
+  }
+}
+
+async function isTodoOwner(req, res, next) {
+  try {
+    const todo = await todoService.findOne(req.params.id);
+    if (!todo) {
+      // #swagger.responses[404] = {description: 'Todo not found', schema: {status: "fail", data: {statusCode: 404, result: "Todo not found"}}}
+      return res.status(404).jsend.fail({ statusCode: 404, result: "Todo not found" });
+    }
+
+    if (todo.UserId !== req.user.id) {
+      return res.status(403).jsend.fail({ statusCode: 403, result: "You are not authorized to delete this todo" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(500).jsend.error({ status: "error", message: "Internal server error", data: error });
+  }
+}
+
 module.exports = {
   isAuth,
   isCategoryOwner,
+  validateBody,
+  validateCategory,
+  validateStatus,
+  isTodoOwner,
 };
