aaad8e8a737bf1849247c11594b7338efc0b2bd3
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-01-10 21:47:20 +0100

    added additional 5 tests

--- tests/Todos.test.js
+++ tests/Todos.test.js
@@ -4,13 +4,14 @@ var jsend = require("jsend");
 var usersRouter = require("../routes/users");
 var categoriesRouter = require("../routes/categories");
 var todosRouter = require("../routes/todos");
-var { createDatabase, db } = require("../models");
+var { createDatabase } = require("../models");
 var crypto = require("crypto");
 const request = require("supertest");
 const createStatuses = require("../utilities/createStatuses");
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+//basic app setup
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,18 +23,39 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-let user;
-const name = "tester";
-const email = "test@test.com";
-const password = "password";
+// Logging in with a valid account.
+// Using the token from 1. to get all the users Todos.
+// Using the token from 1. and add a new Todo item.
+// Deleting the created Todo item from number 3.
+// Trying to get Todos without sending JWT token in the header.
+// Trying to get Todos by sending an invalid JWT token.
 
 describe("Todo Tests", () => {
+  // Variables to be used in the tests
+  let user;
+  let category;
+  let token;
+  let statuses;
+  let todo;
+  let todo2;
+  const name = "tester";
+  const email = "test@test.com";
+  const password = "password";
+  let db = {};
+
   beforeAll(async () => {
-    // Using existing db but could uncomment and use a different db
-    // const options = new Sequelize(process.env.TEST_DATABASE_NAME, process.env.TEST_ADMIN_USERNAME, process.env.TEST_ADMIN_PASSWORD, { host: process.env.TEST_HOST, dialect: process.env.DIALECT });
-    // const db = await createDatabase(options);
+    // Create a new database for testing
+    const options = new Sequelize(
+      process.env.TEST_DATABASE_NAME || process.env.DATABASE_NAME,
+      process.env.TEST_ADMIN_USERNAME || process.env.ADMIN_USERNAME,
+      process.env.TEST_ADMIN_PASSWORD || process.env.ADMIN_PASSWORD,
+      { host: process.env.TEST_HOST || process.env.HOST, dialect: process.env.DIALECT, logging: false }
+    );
+    db = createDatabase(options);
     await db.sequelize.sync({ force: false });
     await createStatuses(db);
+
+    // Create a new user for testing
     const salt = crypto.randomBytes(16);
     const encryptedPassword = crypto.pbkdf2Sync(password, salt, 310000, 32, "sha256");
     user = await db.User.create({
@@ -41,6 +63,24 @@ describe("Todo Tests", () => {
       email,
       salt,
       encryptedPassword,
+    });
+
+    // create a new category for testing with the user id
+    category = await db.Category.create({
+      name: "test",
+      UserId: user.id,
+    });
+
+    // get statuses
+    statuses = await db.Status.findAll();
+
+    // create a new todo for testing
+    todo = await db.Todo.create({
+      name: "test",
+      description: "test",
+      StatusId: statuses[0].id,
+      CategoryId: category.id,
+      UserId: user.id,
     });
   });
 
@@ -52,11 +92,71 @@ describe("Todo Tests", () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.status).toBe("success");
     expect(response.body.data.statusCode).toBe(200);
-    expect(response.body.data.result).toBe("You are logged in");
-    expect(response.body.data.token).toBeDefined();
+    expect(response.body.data.result.id).toBe(user.id);
+    expect(response.body.data.result.name).toBe(user.name);
+    expect(response.body.data.result.token).toBeDefined();
+
+    token = response.body.data.result.token;
+  });
+
+  test("Get all the users Todos", async () => {
+    const response = await request(app).get("/todos").set("Authorization", `Bearer ${token}`);
+    console.log(response.body);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe("success");
+    expect(response.body.data.statusCode).toBe(200);
+    expect(response.body.data.result).toBeDefined();
+    expect(response.body.data.result[0].name).toBe("test");
+    expect(response.body.data.result[0].description).toBe("test");
+  });
+
+  test("Add a new Todo item", async () => {
+    const response = await request(app).post("/todos").set("Authorization", `Bearer ${token}`).send({
+      name: "test2",
+      description: "test2",
+      statusId: statuses[0].id,
+      categoryId: category.id,
+    });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.status).toBe("success");
+    expect(response.body.data.statusCode).toBe(201);
+    expect(response.body.data.result.name).toBe("test2");
+    expect(response.body.data.result.description).toBe("test2");
+    todo2 = response.body.data.result;
+  });
+
+  test("Delete the created Todo item", async () => {
+    const response = await request(app).delete(`/todos/${todo2.id}`).set("Authorization", `Bearer ${token}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe("success");
+    expect(response.body.data.statusCode).toBe(200);
+    expect(response.body.data.result).toBeDefined();
+  });
+
+  test("Get Todos without sending JWT token in the header", async () => {
+    const response = await request(app).get("/todos");
+    console.log(response.body);
+    expect(response.statusCode).toBe(401);
+    expect(response.body.status).toBe("fail");
+    expect(response.body.data.statusCode).toBe(401);
+    expect(response.body.data.result).toBeDefined();
+  });
+
+  test("Get Todos by sending an invalid JWT token", async () => {
+    const response = await request(app).get("/todos").set("Authorization", `Bearer ${token}1`);
+    console.log(response.body);
+    expect(response.statusCode).toBe(401);
+    expect(response.body.status).toBe("fail");
+    expect(response.body.data.statusCode).toBe(401);
+    expect(response.body.data.result).toBeDefined();
   });
 
   afterAll(async () => {
+    // Delete the user and all associated data
+    await db.Todo.destroy({ where: { id: todo.id } });
+    await db.Todo.destroy({ where: { id: todo2.id } });
+    await db.Category.destroy({ where: { UserId: user.id } });
+    await db.User.destroy({ where: { id: user.id } });
     await db.sequelize.close();
   });
 });
